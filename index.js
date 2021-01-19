9fcf5370f250970405e47cbb4f51b7a3aedd2cfe
Husein D <43807768+huseind@users.noreply.github.com>
2021-01-19 18:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-var express = require("express");
-var server = express();
-var options = {
-  index: "index.html",
-};
-server.use("/", express.static("/home/site/wwwroot", options));
-server.listen(3000);
-
-
 const root = document.getElementById('app');
 
 ReactDOM.render(<App />, root);
